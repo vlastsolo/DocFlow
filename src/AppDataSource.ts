f8206aea9a80be6acf90f47fa66a946b67668565
Maxim Solovev <mvs@maxsolo.ru>
2025-08-30 12:51:38 +0600

    Добавил Handlebars в качестве view engine, добавил views, добавил скрипт инициализации БД в docflow.ts

--- src/AppDataSource.ts
+++ src/AppDataSource.ts
@@ -11,4 +11,14 @@ const AppDataSource = new DataSource({
   "subscribers": ["src/subscribers/**/*.ts"]
 });
 
+export async function initializeDatabase() {
+  try {
+    await AppDataSource.initialize();
+    console.log('Data Source has been initialized!');
+  } catch (err) {
+    console.error('Error during Data Source initialization', err);
+    process.exit(1);
+  }
+}
+
 export default AppDataSource;
--- src/docflow.ts
+++ src/docflow.ts
@@ -1,15 +1,42 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import {invoiceRouter} from './routes/invoices';
+import { engine } from 'express-handlebars';
+import AppDataSource from './AppDataSource';
+import { initializeDatabase } from './AppDataSource';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 const app: express.Application = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 3020;
 
+await initializeDatabase();
+
+app.engine('handlebars', engine({
+  extname: '.hbs',
+  defaultLayout: 'main',
+  layoutsDir: path.join(__dirname, 'views/layouts'),
+  partialsDir: path.join(__dirname, 'views/partials'),
+}));
+
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use('/invoices', invoiceRouter)
-app.get('/', (req, res) => {
-  res.send('Hello, TypeScript with Express!');
+
+app.get('/home', (req, res) => {
+  res.render('home', {
+    title: 'О нас',
+    content: 'Добро пожаловать на страницу о нашей компании! Мы работаем на рынке более 10 лет и специализируемся на создании инновационных решений.',
+    companyInfo: {
+      year: 2013,
+      employees: 50,
+}
+  });
 });
 
 app.listen(PORT, () => {
